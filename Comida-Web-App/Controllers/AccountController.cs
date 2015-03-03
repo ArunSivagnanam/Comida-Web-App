8efa58c8036ad4472ca8b39d4bafa958bb7372f3
ArunSivagnanam <Arun.s@live.dk>
2015-03-03 22:02:47 +0100

    firstComit

---
+++ Comida-Web-App/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comida_Web_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Comida-Web-App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comida_Web_App.Startup))]
+namespace Comida_Web_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
